5a7c0f60c0fe95dbcd5ea421f9954e42c221ee5a
malvikabadal <malvikabadal@hotmail.com>
2024-11-11 16:16:20 +0100

    added commando square
    added commando square and made changes for it to work

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,3 +1,9 @@
-import square
+import just_count.square
 
-print(f"The square of 5 is {square.square(5)}")
+
+def main():
+    print(f"The square of 5 is {just_count.square.square(5)}")
+
+
+if __name__ == "__main__":
+    main()
