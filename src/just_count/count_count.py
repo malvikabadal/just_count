a14f669f482ae85dbcdc8823d19bfd90ddf436b0
malvikabadal <malvikabadal@hotmail.com>
2024-11-14 09:05:09 +0100

    changed the import name

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,8 +1,8 @@
-import just_count.square
+import just_count.square as square
 
 
 def main():
-    print(f"The square of 5 is {just_count.square.square(5)}")
+    print(f"The square of 5 is {square.square(5)}")
 
 
 if __name__ == "__main__":
